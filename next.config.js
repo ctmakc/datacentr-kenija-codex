87933ca7130ba4003cb6dc4bfb1c0949605e6138
ctmakc <ctmakc@gmail.com>
2026-02-10 12:13:50 -0500

    fix: stabilize local next runtime and cache directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: ".next-local",
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 module.exports = nextConfig;
